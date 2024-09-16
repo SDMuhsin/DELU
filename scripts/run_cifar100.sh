e7a7fa0ffadf399e0370206bbea46381269d322e
sdmuhsin <sdmuhsin@gmail.com>
2024-09-16 07:03:45 -0600

    Extended epochs for CIFAR100 10->20

--- scripts/run_cifar100.sh
+++ scripts/run_cifar100.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for seed in 41 42 43 44 45; do
-  parallel -j 3 -u "python source/run_cifar100.py --model resnet18 --epochs 10 --batch-size 64 --lr 0.001 --seed {2} --activation {1}" ::: DELU ReLU LeakyReLU ::: $seed
+for seed in 41; do
+  parallel -j 3 -u "python source/run_cifar100.py --model resnet18 --epochs 20 --batch-size 64 --lr 0.001 --seed {2} --activation {1}" ::: DELU ReLU LeakyReLU ::: $seed
 done
