66153a7728c167a8c3af4880ff8111ad3298cdba
sdmuhsin <sdmuhsin@gmail.com>
2024-09-19 11:48:27 -0600

    Scripts prepped for alliance

--- scripts/run_cifar100.sh
+++ scripts/run_cifar100.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mapfile -t activations < ./metadata/activations.txt
 
-for seed in 41; do
-  parallel -j 2 -u "python source/run_cifar100.py --model resnet18 --epochs 30 --batch-size 128 --lr 0.001 --seed {2} --activation {1}" ::: "${activations[@]}" ::: $seed
+for seed in 41 42 43 44 45; do
+  parallel -j 3 -u "python source/run_cifar100.py --model resnet18 --epochs 50 --batch-size 128 --lr 0.001 --seed {2} --activation {1}" ::: "${activations[@]}" ::: $seed
 done
--- scripts/run_svhn.sh
+++ scripts/run_svhn.sh
@@ -3,5 +3,5 @@
 mapfile -t activations < ./metadata/activations.txt
 
 for seed in 41 42 43 44 45; do
-  parallel -j 3 -u "python source/run_svhn.py --model resnet18 --epochs 30 --batch-size 64 --lr 0.001 --seed {2} --activation {1}" ::: "${activations[@]}" ::: $seed
+  parallel -j 3 -u "python source/run_svhn.py --model resnet18 --epochs 50 --batch-size 128 --lr 0.001 --seed {2} --activation {1}" ::: "${activations[@]}" ::: $seed
 done
