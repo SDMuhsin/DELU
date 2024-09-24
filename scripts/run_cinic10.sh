52180e0c87f1cbf08d05fabb8c61c588b0154ba1
sdmuhsin <sdmuhsin@gmail.com>
2024-09-24 07:05:06 -0600

    Added conda env activation to all scripts

--- scripts/run_cinic10.sh
+++ scripts/run_cinic10.sh
@@ -7,6 +7,6 @@ b=${2:-1}  # Use second argument if provided, otherwise default to 1
 export a
 export b
 for seed in 41 42 43 44 45; do
-  parallel -j 1 -u "python source/run_cinic10.py --task cinic10 --model resnet18 --epochs 30 --batch-size 128 --lr 0.001 --seed {2} --activation {1} --a $a --b $b" ::: "${activations[@]}" ::: $seed
+  parallel -j 1 -u "conda activate double_env_6;python source/run_cinic10.py --task cinic10 --model resnet18 --epochs 30 --batch-size 128 --lr 0.001 --seed {2} --activation {1} --a $a --b $b" ::: "${activations[@]}" ::: $seed
   #python source/run_cinic10.py --model resnet18 --epochs 30 --batch-size 128 --lr 0.001 --seed 41 --activation ReLU
 done
--- scripts/run_fmnist.sh
+++ scripts/run_fmnist.sh
@@ -7,5 +7,5 @@ b=${2:-1}  # Use second argument if provided, otherwise default to 1
 export a
 export b
 for seed in 41 42 43 44 45; do
-  parallel -j 1 -u "python source/run_fmnist.py --model resnet18 --task fmnist --epochs 10 --batch-size 64 --lr 0.001 --seed {2} --activation {1} --a $a --b $b" ::: "${activations[@]}" ::: $seed
+  parallel -j 1 -u "conda activate double_env_6;python source/run_fmnist.py --model resnet18 --task fmnist --epochs 10 --batch-size 64 --lr 0.001 --seed {2} --activation {1} --a $a --b $b" ::: "${activations[@]}" ::: $seed
 done
