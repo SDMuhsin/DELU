c1bc9f6592849ffd3e6d701cf6216bacd8e29226
sdmuhsin <sdmuhsin@gmail.com>
2024-09-24 10:55:51 -0600

    EMNIST epoch+=1=

--- scripts/run_emnist.sh
+++ scripts/run_emnist.sh
@@ -10,5 +10,5 @@ export b
 echo $a $b
 
 for seed in 41 42 43 44 45; do
-  parallel -j 1 -u "conda activate double_env_6;conda activate double_env_6;python source/run_emnist.py --task emnist --model resnet18 --epochs 1 --batch-size 128 --lr 0.001 --seed {2} --activation {1} --a $a --b $b" ::: "${activations[@]}" ::: $seed
+  parallel -j 1 -u "conda activate double_env_6;conda activate double_env_6;python source/run_emnist.py --task emnist --model resnet18 --epochs 10 --batch-size 128 --lr 0.001 --seed {2} --activation {1} --a $a --b $b" ::: "${activations[@]}" ::: $seed
 done
