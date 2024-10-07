035d4a019cf25e79fa2e3c730ee7890b0ecb7e1b
sdmuhsin <sdmuhsin@gmail.com>
2024-10-07 07:43:10 -0600

    Unable to get alexnet running

--- source/common/utility.py
+++ source/common/utility.py
@@ -54,95 +54,101 @@ from torchvision import datasets, transforms, models
 def get_model(model_name, dataset):
 
     num_classes = {
-
         'mnist': 10,
-
         'fmnist': 10,
-
         'cifar10': 10,
-
         'cifar100': 100,
-
         'svhn': 10,
-
         'stl10': 10,
-
         'emnist': 47,
-
         'kmnist': 10
-
     }
 
-
-
     if dataset not in num_classes:
-
         raise ValueError(f"Unsupported dataset: {dataset}")
 
-
-
     output_dim = num_classes[dataset]
-
     input_channels = 1 if dataset in ['mnist', 'fmnist', 'emnist', 'kmnist'] else 3
 
-
-
     if model_name == 'resnet18':
-
         model = models.resnet18(pretrained=False)
-
         num_ftrs = model.fc.in_features
-
         model.fc = nn.Linear(num_ftrs, output_dim)
-
+    elif model_name == 'alexnet':
+        model = models.alexnet(pretrained=False)
+        
+        # Modify the first convolutional layer to accept different input channels
+        model.features[0] = nn.Conv2d(input_channels, 64, kernel_size=11, stride=4, padding=2)
+        
+        # Modify the classifier to output the correct number of classes
+        num_ftrs = model.classifier[6].in_features
+        model.classifier[6] = nn.Linear(num_ftrs, output_dim)
+        
+        # Adjust the model for smaller input sizes if necessary
+        if dataset in ['mnist', 'fmnist', 'emnist', 'kmnist', 'cifar10', 'cifar100']:
+            model.features[0].stride = (1, 1)
+            model.features[0].padding = (2, 2)
+            model.features[3].kernel_size = (3, 3)
+            model.features[3].stride = (1, 1)
+            model.features[3].padding = (1, 1)
+            
+            # Remove the second max pooling layer
+            new_features = nn.Sequential(*list(model.features.children())[:-3])
+            model.features = new_features
+            
+            # Adjust the classifier for the new feature map size
+            model.classifier = nn.Sequential(
+                nn.Dropout(),
+                nn.Linear(256 * 6 * 6, 4096),
+                nn.ReLU(inplace=True),
+                nn.Dropout(),
+                nn.Linear(4096, 4096),
+                nn.ReLU(inplace=True),
+                nn.Linear(4096, output_dim),
+            )
     elif model_name == 'resnet34':
 
         model = models.resnet34(pretrained=False)
-
         num_ftrs = model.fc.in_features
         if(dataset in ['kmnist','emnist']):
-
              model.conv1 = nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
         model.fc = nn.Linear(num_ftrs, output_dim)
+
+    elif model_name == 'resnet50':
+
+        model = models.resnet50(pretrained=False)
+        num_ftrs = model.fc.in_features
+        if(dataset in ['kmnist','emnist']):
+             model.conv1 = nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        model.fc = nn.Linear(num_ftrs, output_dim)
+
 
     elif model_name == 'shufflenet':
 
         model = models.shufflenet_v2_x0_5(pretrained=False)
-
         if(dataset in ['kmnist','emnist']):
-
             model.conv1[0] = nn.Conv2d(input_channels, 24, kernel_size=3, stride=2, padding=1, bias=False)
-
         num_ftrs = model.fc.in_features
-
         model.fc = nn.Linear(num_ftrs, output_dim)
 
     elif model_name == 'vgg16':
 
         model = models.vgg16(pretrained=False)
-
         model.features[0] = nn.Conv2d(input_channels, 64, kernel_size=3, padding=1)
-
         num_ftrs = model.classifier[6].in_features
-
         model.classifier[6] = nn.Linear(num_ftrs, output_dim)
 
     elif model_name == 'densenet121':
 
         model = models.densenet121(pretrained=False)
-
         model.features.conv0 = nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
-
         num_ftrs = model.classifier.in_features
-
         model.classifier = nn.Linear(num_ftrs, output_dim)
 
     elif model_name == 'smallnet':
 
         model = SmallNet()
-
         num_ftrs = model.classifier.in_features
-
         model.classifier = nn.Linear(num_ftrs, output_dim)
 
     else:
